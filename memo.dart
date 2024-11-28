468d3adbb756da55a8ac0c99536ef701c4875432
taewook Kim <codelist829@naver.com>
2024-11-28 17:51:16 +0900

    좀돼라 시발롬아

--- memo.dart
+++ memo.dart
@@ -15,5 +15,10 @@ class Memo {
     return "id : $id, name : $name, content : $content";
   }
 }
-
+void main(){
+  var xodnr = Memo(content: "7번쨰~", name: "rtx_102");
+  String str = xodnr.toString();
+  print(str);
+  //왜 안될까요? tlqkf
+}
 // 이 주석은 피시방에서 추가한 주석입니다~ 잘 되는데??
